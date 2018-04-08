f8a7c8fefba4432072e4dae6bdc8a2f9b1ff4096
biozz <ielfimov@gmail.com>
2018-04-08 15:56:24 +0300

    Add example blocks

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"go.uber.org/zap"
 	"net/http"
 	"time"
+	"fmt"
 )
 
 const (
@@ -14,15 +14,37 @@ const (
 func main() {
 	ergoNodeClient := initializeClient()
 	//resp, err := ergoNodeClient.GetBlocks(&GetBlocksRequest{Offset: 10000})
-	resp, err := ergoNodeClient.GetBlock("Vumq5gex8Ty3TuAk8Xxxc9UmRgRd64pnRxvV3PM7Q4Q")
+	//resp, err := ergoNodeClient.GetBlock("Vumq5gex8Ty3TuAk8Xxxc9UmRgRd64pnRxvV3PM7Q4Q")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//decoded, err := DecodeToBig([]byte(resp.Header.Id))
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Printf("%b\n", decoded)
+	resp, err := ergoNodeClient.GetBlocks(&GetBlocksRequest{Limit: 50})
 	if err != nil {
 		panic(err)
 	}
-	decoded, err := DecodeToBig([]byte(resp.Header.Id))
-	if err != nil {
-		panic(err)
+
+	blocks := make([]Block, 0)
+	for i := 0; i < len(resp.BlocksIds); i++ {
+		block, err := ergoNodeClient.GetBlock(string(resp.BlocksIds[i]))
+		if err != nil {
+			panic(err)
+		}
+		blocks = append(blocks, *block)
 	}
-	fmt.Printf("%b\n", decoded)
+	proofs, lastBlocks := Prove(&Chain{blocks})
+
+	verified, proofs := Verify(proofs, lastBlocks)
+
+	if verified {
+		fmt.Println("success")
+	} else {
+		fmt.Println("failure")
+	}
 }
 
 func initializeClient() *ErgoNodeClient {
