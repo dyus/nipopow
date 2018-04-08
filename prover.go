97af1175483b0742e84100eb0eb8fe073bf8ac88
bkhasanov <bkhasanov@yandex-team.ru>
2018-04-08 15:58:57 +0300

    update prover

--- prover.go
+++ prover.go
@@ -1,6 +1,9 @@
 package main
 
-import "math/big"
+import (
+	"math"
+	"math/big"
+)
 
 const (
 	k     = 6
@@ -8,17 +11,61 @@ const (
 	delta = 0.5
 )
 
-func IsLocallyGoodSuperchain(chain *Chain, alpha []Block, level int) bool {
-	powered := big.Int{}.Exp(big.NewInt(int64(2)), big.NewInt(int64(level)), nil)
-	right := big.Float{}.Quo(big.NewFloat(float64((1-delta)*float64(len(chain.blocks)))), big.Float{}.SetInt(powered))
+func IsLocallyGoodSuperchain(chain []Block, alpha []Block, level int) bool {
+	powered := big.NewInt(int64(2))
+	powered.Exp(powered, big.NewInt(int64(level)), nil)
+
+	right := &big.Float{}
+	right.Quo(big.NewFloat(float64((1-delta)*float64(len(chain)))), (&big.Float{}).SetInt(powered))
 	return big.NewFloat(float64(len(alpha))).Cmp(right) > 0
 }
 
+func filterByLevel(chain []Block, level int) *[]Block {
+	filteredBlocks := make([]Block, 0)
+
+	for _, block := range chain {
+		if block.GetLevel() >= level {
+			filteredBlocks = append(filteredBlocks, block)
+		}
+	}
+	return &filteredBlocks
+}
+
 func IsHaveSuperchainQuality(chain *Chain, alpha []Block, level int) bool {
+	for mS := m; mS < len(alpha); mS++ {
+		chainSlice := chain.blocks[len(chain.blocks)-mS:]
+		checkChain := make([]Block, 0)
+		reversedCheckChain := make([]Block, 0)
+
+		for i, block := range chainSlice {
+			if block.GetLevel() >= level {
+				checkChain = append(checkChain, block)
+			}
+
+			reversedBlock := checkChain[len(checkChain)-1-i]
+			if reversedBlock.GetLevel() >= level {
+				reversedCheckChain = append(reversedCheckChain, reversedBlock)
+			}
+		}
+
+		if !IsLocallyGoodSuperchain(checkChain, reversedCheckChain, level) {
+			return false
+		}
+	}
 	return true
 }
 
 func IsHaveMultilevelQuality(chain *Chain, alpha []Block, level int) bool {
+	const k1 = m
+	for levelS := level; levelS > 0; levelS-- {
+		// Any nested chain
+		filteredByLevelSCount := len(*filterByLevel(chain.blocks, levelS))
+		if filteredByLevelSCount >= k1 {
+			if float64(len(*filterByLevel(chain.blocks, level))) < (1-delta)*float64(filteredByLevelSCount)*math.Pow(2, float64(level-levelS)) {
+
+			}
+		}
+	}
 	return true
 }
 
@@ -34,7 +81,7 @@ func Prove(chain *Chain) ([]Block, []Block) {
 	for level := initialLevel; level >= 0; level-- {
 		alpha := make([]Block, 0, len(chain.blocks)-k)
 		for i := 0; i < len(chain.blocks)-k; i++ {
-			if chain.blocks[i].Header.Height >= block.Header.Height && chain.blocks[i].GetLevel() >= level {
+			if block.Header.Height <= chain.blocks[i].Header.Height && chain.blocks[i].GetLevel() >= level {
 				alpha = append(alpha, chain.blocks[i])
 			}
 		}
